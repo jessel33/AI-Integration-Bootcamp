c1357a174590db2d8200851578caf4d65314db1e
Jesse Armstrong <235686176+jessel33@users.noreply.github.com>
2026-06-12 01:43:33 -0400

    Add README file into contact_manage project and start writing the contact.py contact class.

--- Week1/contact_manage/contact.py
+++ Week1/contact_manage/contact.py
@@ -1,1 +1,18 @@
 # Contact.py file
+# Create a Contact class that can:
+# - store name, email, phone, address, and notes
+# - update its own fields
+# - return a readable string version
+# - convert itself to a dictionary
+
+class Contact:
+    def __init__(self, name, email, phone, address, notes):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.address = address
+        self.notes = notes
+
+    def get_info(self):
+        return f"Name: {self.name} Email: {self.email} Phone: {self.phone} Address: {self.address} Notes: {self.notes}"
+    
