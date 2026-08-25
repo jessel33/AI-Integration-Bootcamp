07ce2dcf27d72ef6cc97fe39c25590710d480216
jessel33 <etown33hammer@gmail.com>
2026-08-24 21:45:20 -0400

    build django models, edit settings and urls files, connect to MySQL database and built the tables/models.

--- Week3/customer_support_ticket_system/config/settings.py
+++ Week3/customer_support_ticket_system/config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Cust_Support_Ticket_Sys',
+        'USER': 'root',
+        'PASSWORD': 'CB900f#33',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- Week3/customer_support_ticket_system/tickets/models.py
+++ Week3/customer_support_ticket_system/tickets/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.db.models import CASCADE
+from django.db.models.fields import DateTimeField
 
-# Create your models here.
+class Customer(models.Model):
+    customer_id = models.IntegerField(primary_key=True)
+    customer_name = models.CharField(max_length=40, unique=True)
+    street = models.CharField(max_length=80)
+    city = models.CharField(max_length=40)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=20)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=60, unique=True)
+
+    def __str__(self):
+        return f"{self.customer_name}"
+
+
+class ServiceRepresentative(models.Model):
+    service_rep_id = models.IntegerField(primary_key=True)
+    service_rep_name = models.CharField(max_length=40, unique=True)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=60, unique=True)
+    start_date = models.DateTimeField(editable=True)
+    specialization = models.TextField(max_length=150)
+
+    def __str__(self):
+        return f"{self.service_rep_name}"
+  
+
+class SupportTicket(models.Model):
+  COMPL_STATUS = [
+     ('Pending', 'Pending'),
+     ('In Progress', 'In Progress'),
+     ('Completed', 'Completed'),
+     ('Cancelled', 'Cancelled'),
+  ]
+  ticket_id = models.IntegerField(primary_key=True)
+  model_name = models.CharField(max_length=80)
+  model_number = models.IntegerField()
+  scope_of_work = models.TextField(max_length=1000)
+  ticket_creation_timestamp = models.DateTimeField(auto_created=True)
+  customer_id = models.ForeignKey(Customer, on_delete=models.RESTRICT)
+  service_rep_id = models.ForeignKey(ServiceRepresentative, on_delete=models.RESTRICT)
+  service_rep_notes = models.TextField(max_length=1000)
+  repair_completion = models.CharField(choices=COMPL_STATUS, max_length=20, default='pending')
+
+  def __str__(self):
+     return f"{self.ticket_id}"
+  
+
+class HistTicketRecord(models.Model):
+  ticket_id = models.ForeignKey(SupportTicket, on_delete=models.CASCADE)
+  service_rep_id = models.ForeignKey(ServiceRepresentative, on_delete=models.RESTRICT)
+  ticket_assigned_timestamp = models.DateTimeField(editable=True)
+  ticket_completion_timestamp = models.DateTimeField(editable=True)
+
+  def __str__(self):
+    return f"{self.ticket_id}"
+
+
